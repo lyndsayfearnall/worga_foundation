e2c5e5d0dc8c58fa8dcfbbcabe96497cb50bcca3
lyndsay.fearnall <lyndsay.fearnall@gmail.com>
2020-07-30 22:52:16 -0400

    added handlesubmit

--- src/components/Molecules/NewsletterForm/Newsletter.js
+++ src/components/Molecules/NewsletterForm/Newsletter.js
@@ -13,16 +13,35 @@ const Label = styled.label`
   display: none;
 `
 
-const Newsletter = () => {
+class Newsletter extends React.Component{
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: ""
+//     };
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//   }
+
+//   handleInputChange(event) {
+//     const target = event.target;
+//     const email = target.name;
+// }
+
+handleSubmit = event => {
+  event.preventDefault()
+}
+
+  render(){
   return(
     <NewsForm method="POST" netlify-honeypot="bot-field" data-netlify="true" name="Newsletter" >
       <input type="hidden" name="Newsletter" value="Newsletter" />
       <Label for="email">Email address: &nbsp;</Label>
-      <FormField type="email" placeholder="hello@myemail.com" required></FormField>
+      <FormField type="email" placeholder="hello@myemail.com" required/>
       <input type="hidden" name="bot-field" />
-      <FormField type="submit" value="Sign Me Up"></FormField>
+      <FormField type="submit" value="Sign Me Up" onSubmit={this.handleSubmit}/>
     </NewsForm>
-  )
+    )
+  } 
 }
 
 export default Newsletter
