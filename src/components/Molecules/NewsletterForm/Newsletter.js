09fdd3c897c29c357cc2ea48205f955f7020f3de
lyndsay.fearnall <lyndsay.fearnall@gmail.com>
2020-07-30 22:36:14 -0400

    config form

--- src/components/Molecules/NewsletterForm/Newsletter.js
+++ src/components/Molecules/NewsletterForm/Newsletter.js
@@ -16,11 +16,11 @@ const Label = styled.label`
 const Newsletter = () => {
   return(
     <NewsForm method="POST" netlify-honeypot="bot-field" data-netlify="true" name="Newsletter">
+      <input type="hidden" name="Newsletter" value="contact" />
       <Label for="email">Email address: &nbsp;</Label>
         <FormField type="email" placeholder="hello@myemail.com" required></FormField>
         <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <button type="submit" value="Sign Me Up"></button>
+        <FormField type="submit" value="Sign Me Up"></FormField>
     </NewsForm>
   )
 }
